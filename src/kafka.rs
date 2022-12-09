0033ea22a939b3d6d3257c6af123ba6c50ddba8d
Ryan Ruckley <ryan.ruckley@optus.com.au>
2022-12-09 16:07:34 +1100

    Additional config

--- src/kafka.rs
+++ src/kafka.rs
@@ -4,24 +4,32 @@ use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 
 struct KafkaConfig {
     pub host : String,
+    pub topic : String,
+    pub group : String,
 }
 
 impl ::std::default::Default for KafkaConfig {
     fn default() -> Self {
         Self {
-            host : "https://10.122.13.226:8080".into(),
+            host : "https://10.122.13.226:9092".into(),
+            topic: "events".into(),
+            group: "rust-client".into(),
         }
     }
 }
 
-pub struct kClient {
+pub struct KafkaClient {
     pub consumer : kafka::consumer::Consumer,
 }
 
-impl kClient {
+impl KafkaClient {
     pub fn new() -> Self {
         let config = KafkaConfig::default();
         let consumer = Consumer::from_hosts(vec!(config.host.to_owned()))
+            .with_topic_partitions(config.topic.to_owned(),&[0,1])
+            .with_fallback_offset(FetchOffset::Earliest)
+            .with_group(config.group)
+            .with_offset_storage(GroupOffsetStorage::Kafka)
             .create()
             .unwrap();
         Self {
