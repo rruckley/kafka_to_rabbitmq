db7ffe204847d9240680fa617f43b580ddb27eec
Ryan Ruckley <ryan.ruckley@optus.com.au>
2022-12-09 16:07:42 +1100

    use updated names

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod kafka;
 fn main() {
     println!("Kafka to RabbitMQ!");
 
-    let _r = rabbitmq::rClient::new();
-    let _k = kafka::kClient::new();
+    let _r = rabbitmq::RabbitClient::new();
+    let _k = kafka::KafkaClient::new();
 }
