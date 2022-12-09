5de9d2ed9d289aae219273da767c3982d127b986
Ryan Ruckley <ryan.ruckley@optus.com.au>
2022-12-09 17:17:55 +1100

    Open channel

--- src/rabbitmq.rs
+++ src/rabbitmq.rs
@@ -3,26 +3,39 @@ use amqp::{Session};
 
 struct RabbitMQConfig {
     url : String,
+    vhost : String,
 }
 
 impl ::std::default::Default for RabbitMQConfig {
     fn default() -> Self {
         Self {
-            url : "amqp://10.122.13.226:5672".into(),
+            url : "amqp://10.122.13.226:5672/evt".into(),
+            vhost: "evt".into(),
         }
     }
 }
 
 pub struct RabbitClient {
     session : amqp::Session,
+    channel : amqp::Channel,
 }
 
 impl RabbitClient {
     pub fn new() -> Self {
         let config = RabbitMQConfig::default();
-        let session = Session::open_url(&config.url).expect("Could not connecto to RabbitMQ");
+        let mut session = Session::open_url(&config.url)
+            .expect("Could not connecto to RabbitMQ");
+        let channel = session.open_channel(1).expect("Could not open channel");
+        println!("Using url: {} and vhost: {}",config.url,config.vhost);
         Self {
             session,
+            channel,
         }
+    }
+
+    pub fn queue(self : &Self, key: String, value : String) -> Option<i32> {
+        // Do something with key and value
+        println!("Queuing: {} {}",key,value);
+        Some(1)
     }
 }
